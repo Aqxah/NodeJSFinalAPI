27bdc1fb377d16706d8935501d5d17a75c2f7489
Tanner Legleiter <tanner.legleiter@gmail.com>
2024-05-05 11:59:17 -0500

    Test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const path = require('path');
-const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
@@ -18,7 +18,7 @@ connectDB();
 app.use(logger);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors())
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
